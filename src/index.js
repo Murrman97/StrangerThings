b013854104b80a42e5ec879172cba479a3564d88
konradtbudny <konradtbudny@gmail.com>
2022-03-19 18:57:56 -0500

    To-Do list update

--- src/index.js
+++ src/index.js
@@ -21,8 +21,9 @@ ReactDOM.render(
 Things to do:
 1) EditPost does not actually update a post
 2) profile throws up an error Cannot read properties of undefined, when the app is refreshed or at first enter to profile tab
-3) message css
-4) when Loging out and loging in once again, userObj holds values for previous user(components: home and profile)
+3) Profile display wrong posts for a user(at least when user is changed)
+4) message css
+5) when Loging out and loging in once again, userObj holds values for previous user(components: home and profile)
 
 Notes:
 1) Editing a post is possible from profile
